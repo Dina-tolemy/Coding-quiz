ff5e6ae0d679125b027f6fa9572c2725e6b16124
Dinatolemy <dina.a.tolemy@gmail.com>
2019-11-28 19:09:01 +1100

    adding css

--- assets/javascript.js
+++ assets/javascript.js
@@ -8,10 +8,15 @@ var secondAnswer = document.getElementById("B");
 var thirdAnswer = document.getElementById("C");
 var fourthAnswer = document.getElementById("D");
 var rightAnswer = document.getElementById("answer");
-var result = document.getElementById("resultContainer");
+var resultTotal = document.getElementById("resultContainer");
+var result = document.getElementById("result");
 var timer = document.getElementById("timer");
-var counter = document.getElementById("counter");
+var SubmitBtn = document.getElementById("submit");
 var counter1 = document.getElementById("counter1");
+var highScoreBar = document.getElementById("High-Score");
+
+
+
 
 // the questions Array //
 var questionArray = [{
@@ -57,30 +62,25 @@ var questionArray = [{
 
 var lastQuestion = questionArray.length - 1;
 var currentQuestion = 0;
-var count = 0;
-var questionTime = 10;
 var score = 0;
-var totalTime=50;
-var count2=0;
+var totalTime = 70;
+var count2 = 0;
 var totalTimer;
 startQuizButton.addEventListener("click", startQuizFunc);
-
-
 
 //starting the quiz function 
 
 function startQuizFunc() {
 
+
     startQuiz.style.display = "none";
+    resultTotal.style.display="block";
     displayQuestion();
-    displayCounter();
-    displayResult();
-    timer = setInterval(displayCounter, 1000);
-    totalTimer=setInterval(displayTotalTime,1000);
-
-
+    totalTimer = setInterval(displayTotalTime, 1000);
     quizQues.style.display = "block";
-    result.style.display = "block";
+    counter1.style.display="inline-block";
+    highScoreBar.style.display="inline-block";
+  
 
 }
 // display the question function
@@ -94,59 +94,20 @@ function displayQuestion() {
     secondAnswer.textContent = q.secondAnswer;
     thirdAnswer.textContent = q.thirdAnswer;
     fourthAnswer.textContent = q.fourthAnswer;
+    
 }
 
 //display the result 
 
-function displayResult() {
-
-    for (var qIndex = 0; qIndex <= lastQuestion; qIndex++) {
-
-
-    }
-}
-
-// displaying the time for each question function
-
-function displayCounter() {
-
-    if (count <= questionTime) {
-        counter.textContent = count;
-        count++;
-        
-    }
-    else if (currentQuestion < lastQuestion) {
-
-        score = score;
-        result.textContent =  "your score is still " + score;
-        count = 0;
-        currentQuestion++;
-        displayQuestion();
-
-    }
-    else {
-
-        clearInterval(timer);
-        showScore();
-
-    }
-    
 
 
 
-
-}
-
-// total
-
-
-function displayTotalTime()
-{
-    if(count2<totalTime){
+function displayTotalTime() {
+    if (count2 < totalTime) {
 
         totalTime--;
-        counter1.textContent=totalTime;
-        
+        counter1.textContent = totalTime;
+
     }
 
 }
@@ -157,58 +118,70 @@ function ckeckAnsewr(answer) {
 
     if (answer == questionArray[currentQuestion].rightAnswer) {
         score += 10;
-        rightAnswer.textContent="correct answer";
-        result.textContent = "your score is " + score;
-        count = 0;
+        rightAnswer.textContent = "correct answer";
+      
     }
     else {
-
-        score = score;
-        result.textContent = "your score is still " + score;
-        rightAnswer.textContent="Wrong answer";
-        count = 0;
-    }
-
-    if (currentQuestion < lastQuestion) {
-
-        currentQuestion++;
-        displayQuestion();
+        totalTime = totalTime - 10;
+        rightAnswer.textContent = "Wrong answer!";
 
 
     }
-    else {
 
-        clearInterval(timer);
-        showScore();
-    }
+if (currentQuestion < lastQuestion) {
+
+    currentQuestion++;
+    displayQuestion();
+
+
+}
+else {
+
+    clearInterval(totalTime);
+    showScore();
+}
 
 }
 
-// showing final score
-
+//showing final score
 function showScore() {
 
-    quizQues.style.display = "none";
     
-clearInterval(totalTimer);
-    if (totalTime > 30) {
-
-        score += 20;
-        result.textContent = "Well done you got extra points for finishing in less that 20 sec your final score is " + score;
-
-
-    }
-    else if (totalTime > 20) {
-
-        score += 10;
-        result.textContent = "Good job you got extra 10 points for finishing in less than 30 sec your final score is " + score;
-    }
-    else {
-        score = score;
-        result.textContent = "Good job your total score is " + score;
-
-    }
-
+    quizQues.style.display = "none";
+    resultTotal.style.display="block";
+    clearInterval(totalTimer);
+    score = score + totalTime;
+    result.textContent="your final score is "+score;
+  
+    return score;
+    
 }
+
+//checking the highest score
+
+function submitFunction(){
+   
+
+   var userInitial=document.getElementById("userName").value;
+  //  console.log(userInitial);
+   var  lastUserScore =score;
+  //  console.log(lastUserScore);
+  
+  localStorage.setItem('highscore', lastUserScore);
+  var scorefinal = localStorage.getItem('highscore');
+
+  localStorage.setItem('initials', userInitial);
+  var nameFinal = localStorage.getItem('initials');
+  highScoreBar.innerHTML=scorefinal;
+
+  
+var Scorebar=document.createElement("div");
+Scorebar.innerHTML= nameFinal+"------------"+scorefinal;
+resultTotal.appendChild(Scorebar); 
+}
+
+
+
+
 
 
