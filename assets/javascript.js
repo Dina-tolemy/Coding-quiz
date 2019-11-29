c6657ed6aa216eb5bc57b8c20ca7256ff0e5a762
Dinatolemy <dina.a.tolemy@gmail.com>
2019-11-30 00:35:15 +1100

    saving the high score

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,3 +1,4 @@
+// reference questions.js
 // defining the variables//
 var startQuiz = document.getElementById("start");
 var startQuizButton = document.getElementById("startQuiz");
@@ -15,54 +16,12 @@ var SubmitBtn = document.getElementById("submit");
 var counter1 = document.getElementById("counter1");
 var highScoreBar = document.getElementById("High-Score");
 var tryAgainBtn = document.getElementById("again");
-var wrongSound=document.getElementById("myAudio");
-var rightSound=document.getElementById("myAudio2");
+var wrongSound = document.getElementById("myAudio");
+var rightSound = document.getElementById("myAudio2");
 
 
 
 // the questions Array //
-var questionArray = [{
-    theQuestion: "Javascript is _________ language.",
-    firstAnswer: "Scripting",
-    secondAnswer: "Programing",
-    thirdAnswer: "texting",
-    fourthAnswer: "Application",
-    rightAnswer: "A"
-}, {
-    theQuestion: "Inside which HTML element do we put the JavaScript?",
-    firstAnswer: "<js>",
-    secondAnswer: "<title>",
-    thirdAnswer: "<script>",
-    fourthAnswer: "<head>",
-    rightAnswer: "C"
-}, {
-    theQuestion: " When a user views a page containing a JavaScript program, which machine actually executes the script?",
-    firstAnswer: "The User's machine running a Web browser",
-    secondAnswer: " The Web server",
-    thirdAnswer: "A central machine deep within Netscape's corporate offices",
-    fourthAnswer: "None of the above",
-    rightAnswer: "A"
-}, {
-    theQuestion: " What are variables used for in JavaScript Programs?",
-    firstAnswer: " Varying randomly",
-    secondAnswer: "Storing numbers, dates, or other values",
-    thirdAnswer: " Causing high-school algebra flashbacks",
-    fourthAnswer: "None of the above",
-    rightAnswer: "B"
-}, {
-    theQuestion: " Which of the following can't be done with client-side JavaScript?",
-    firstAnswer: "Validating a form",
-    secondAnswer: "Sending a form's contents by email",
-    thirdAnswer: "Storing the form's contents to a database file on the server",
-    fourthAnswer: "None of the above",
-    rightAnswer: "C"
-}
-
-
-];
-
-
-var highscore = 0;
 var userScore = 0;
 var lastQuestion = questionArray.length - 1;
 var currentQuestion = 0;
@@ -73,60 +32,38 @@ startQuizButton.addEventListener("click", startQuizFunc);
 SubmitBtn.addEventListener("click", submitFunction);
 tryAgainBtn.addEventListener("click", startQuizFunc);
 
-
-
-//starting the quiz function 
-
 function startQuizFunc() {
-
     lastQuestion = questionArray.length - 1;
     currentQuestion = 0;
     totalTime = 75;
     count2 = 0;
-
     startQuiz.style.display = "none";
-    // resultTotal.style.display = "block";
     displayQuestion();
     totalTimer = setInterval(displayTotalTime, 1000);
     quizQues.style.display = "block";
     counter1.style.display = "inline-block";
     highScoreBar.style.display = "inline-block";
 
-
-
-
 }
 // display the question function
 
 function displayQuestion() {
 
-
+    resultTotal.style.display = "none";
     var q = questionArray[currentQuestion];
     theQuestion.textContent = q.theQuestion;
     firstAnswer.textContent = q.firstAnswer;
     secondAnswer.textContent = q.secondAnswer;
     thirdAnswer.textContent = q.thirdAnswer;
     fourthAnswer.textContent = q.fourthAnswer;
-
-
 }
 
 //display the result 
-
-
-
-
 function displayTotalTime() {
     if (count2 < totalTime) {
-
         totalTime--;
         counter1.textContent = totalTime;
-
-    }
-
-    else if (count2 == 0) {
-
-
+    } else if (count2 == 0) {
         clearInterval(totalTimer);
         showScore();
     }
@@ -136,52 +73,32 @@ function displayTotalTime() {
 // ckecking the correct answer function 
 
 function ckeckAnsewr(answer) {
-
     if (answer == questionArray[currentQuestion].rightAnswer) {
-
         rightSound.play();
-
         rightAnswer.textContent = "correct answer";
-
-    }
-    else {
+    } else {
         totalTime = totalTime - 10;
         wrongSound.play();
         rightAnswer.textContent = "Wrong answer!";
-
-
     }
-
     if (currentQuestion < lastQuestion) {
-
         currentQuestion++;
         displayQuestion();
-
-
-    }
-    else {
-
-
+    } else {
         clearInterval(totalTimer);
         counter1.textContent = totalTime;
         showScore();
     }
-
 }
 
 //showing final score
 function showScore() {
-
-
     quizQues.style.display = "none";
     resultTotal.style.display = "block";
     clearInterval(totalTimer);
     userScore = totalTime;
-    highscore = totalTime;
-
+    // highscore = totalTime;
     result.textContent = "your final score is " + userScore;
-
-
 }
 
 
@@ -189,29 +106,29 @@ function submitFunction() {
 
     var userInitial = document.getElementById("userName").value;
     var userInfo = {
-
-        "name": userInitial,
         "score": userScore,
-
-    }
-
-    localStorage.setItem("highscore", highscore);
+        "name": userInitial,
+    };
+    
+    var highscoreArray= localStorage.getItem("highscore") ? JSON.parse(localStorage.getItem("highscore")) : [];
+    highscoreArray.push(userScore);
+    localStorage.setItem("highscore",JSON.stringify(highscoreArray));
     localStorage.setItem("userInfo", JSON.stringify(userInfo));
     var userInformation = JSON.parse(localStorage.getItem("userInfo"));
+   
     var Scorebar = document.createElement("div");
     Scorebar.innerHTML = userInformation.name + "---------------" + userInformation.score;
     resultTotal.appendChild(Scorebar);
 
-    var HighestScore = localStorage.getItem("highscore");
-    if (userInformation.score >= HighestScore) {
-        localStorage.setItem("highscore", userInformation.score);
-        highScoreBar.innerHTML = "Highest score is:  " + userInformation.score;
+    function numberSort(a,b){
+        return a - b;
     }
-    else {
 
-        highScoreBar.innerHTML = "Highest score is:  " + HighestScore;
+    highscoreArray.sort(numberSort);
 
-    }
+    var maxScore = highscoreArray[highscoreArray.length - 1];
+
+    highScoreBar.innerHTML = "highest score is {"+  maxScore  +"}";
 
 
 }
---
+++ assets/questions.js
@@ -0,0 +1,37 @@
+var questionArray = [{
+    theQuestion: "Javascript is _________ language.",
+    firstAnswer: "Scripting",
+    secondAnswer: "Programing",
+    thirdAnswer: "texting",
+    fourthAnswer: "Application",
+    rightAnswer: "A"
+}, {
+    theQuestion: "Inside which HTML element do we put the JavaScript?",
+    firstAnswer: "<js>",
+    secondAnswer: "<title>",
+    thirdAnswer: "<script>",
+    fourthAnswer: "<head>",
+    rightAnswer: "C"
+}, {
+    theQuestion: " When a user views a page containing a JavaScript program, which machine actually executes the script?",
+    firstAnswer: "The User's machine running a Web browser",
+    secondAnswer: " The Web server",
+    thirdAnswer: "A central machine deep within Netscape's corporate offices",
+    fourthAnswer: "None of the above",
+    rightAnswer: "A"
+}, {
+    theQuestion: " What are variables used for in JavaScript Programs?",
+    firstAnswer: " Varying randomly",
+    secondAnswer: "Storing numbers, dates, or other values",
+    thirdAnswer: " Causing high-school algebra flashbacks",
+    fourthAnswer: "None of the above",
+    rightAnswer: "B"
+}, {
+    theQuestion: " Which of the following can't be done with client-side JavaScript?",
+    firstAnswer: "Validating a form",
+    secondAnswer: "Sending a form's contents by email",
+    thirdAnswer: "Storing the form's contents to a database file on the server",
+    fourthAnswer: "None of the above",
+    rightAnswer: "C"
+}
+];
